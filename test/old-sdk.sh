711154b610db0ab2b6e93e2309d52218aaea31cb
nkzawa <naoyuki.kanezawa@gmail.com>
2018-09-10 21:14:57 +0900

    enable to run tests of new cosmos sdk

--- test/old-sdk.sh
+++ test/old-sdk.sh
@@ -6,7 +6,7 @@ trap 'kill -9 $pid' EXIT
 ./bin/cosmosdb-server.js -p "$port" &
 pid=$!
 
-cd test/azure-cosmos-js
+cd test/azure-cosmosdb-node/source
 yarn
-yarn build
-ACCOUNT_HOST="https://localhost:$port" yarn test
+cd test
+ACCOUNT_HOST="https://localhost:$port" ../node_modules/.bin/mocha -t 0 -R spec -b
