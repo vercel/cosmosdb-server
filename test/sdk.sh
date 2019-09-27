cbe3b2c28b39c7fbfcda06cb653dd9d728115f76
nkzawa <naoyuki.kanezawa@gmail.com>
2019-09-27 17:04:37 +0900

    update cosmos sdk for test

--- test/sdk.sh
+++ test/sdk.sh
@@ -6,7 +6,13 @@ trap 'kill -9 $pid' EXIT
 ts-node ./src/cli.ts -p "$port" &
 pid=$!
 
-cd test/azure-cosmos-js
+cd test/azure-sdk-for-js/sdk/cosmosdb/cosmos
 npm install
-ACCOUNT_HOST="https://localhost:$port" npm run test-ts -- -i \
-  -g 'Authorization|database account|http proxy|Change Feed|Cross Partition|indexing|Offer CRUD|Parallel Query As String|Permission|Query Metrics On Single Partition Collection|ResourceLink Trimming|Session Token|spatial|sproc|stored procedure|Trigger|trigger|TTL|User'
+
+cp src/tsconfig.json{,.bak}
+cp ../../../../tsconfig.sdk.patch.json src/tsconfig.json
+npm run build:test -- --force
+mv src/tsconfig.json{.bak,}
+
+ACCOUNT_HOST="https://localhost:$port" npm run integration-test:node -- --i --exit \
+  -g 'Authorization|database account|http proxy|Change Feed|Cross Partition|indexing|Offer CRUD|Parallel Query As String|Permission|Query Metrics On Single Partition Collection|ResourceLink Trimming|Session Token|spatial|sproc|stored procedure|Trigger|trigger|TTL|User|Non Partitioned'
