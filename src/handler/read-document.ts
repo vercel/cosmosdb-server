4d6371b268d43ef24cf9d663f92ecd83de30b0ff
Javi Velasco <javier.velasco86@gmail.com>
2020-01-20 13:49:53 +0100

    Add partitionKey check

--- src/handler/read-document.ts
+++ src/handler/read-document.ts
@@ -20,7 +20,48 @@ export default (
     .collection(collId)
     .document(docId)
     .read();
-  if (!data) {
+
+  const collection = account
+    .database(dbId)
+    .collection(collId)
+    .read();
+
+  // We need to check the collection config so if it's not there it's 404
+  if (!collection) {
+    res.statusCode = 404;
+    return {
+      code: "NotFound",
+      message: "Entity with the specified id does not exist in the system.,"
+    };
+  }
+
+  // We get the paths for the partitionKeys
+  const paths = collection.partitionKey.paths
+    .map(path => path.slice(1))
+    .filter(path => path !== "_partitionKey");
+
+  // Get the partition keys coming from the header
+  const partitionKeys = JSON.parse((req.headers[
+    "x-ms-documentdb-partitionkey"
+  ] || "[]") as string);
+
+  // If there is a mismatch between provided partition keys and collection 400
+  if (paths.length !== partitionKeys.length) {
+    res.statusCode = 400;
+    return {
+      code: "BadRequest",
+      message:
+        "The partition key supplied in x-ms-partitionkey header has fewer components than defined in the collection"
+    };
+  }
+
+  // If the partition keys are not matching the data or there is no data 404
+  if (
+    !data ||
+    paths.some(
+      (path, idx) => (data as { [k: string]: any })[path] !== partitionKeys[idx]
+    )
+  ) {
     res.statusCode = 404;
     return {
       code: "NotFound",
