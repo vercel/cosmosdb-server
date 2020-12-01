f851ef6647eddf7ea155646173bce2576e5fe62b
Mark Glagola <mark.glagola@gmail.com>
2020-12-01 14:55:01 -0600

    Fix replace 409s

--- src/handler/replace-collection.ts
+++ src/handler/replace-collection.ts
@@ -36,7 +36,7 @@ export default async (
   }
 
   try {
-    return database.collections.replace(body, data);
+    return await database.collections.replace(body, data);
   } catch (err) {
     if (err.badRequest) {
       res.statusCode = 400;
@@ -44,7 +44,7 @@ export default async (
     }
     if (err.conflict) {
       res.statusCode = 409;
-      return { message: err.message };
+      return { message: err.message, code: "Conflict" };
     }
 
     throw err;
