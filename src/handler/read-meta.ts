eaab8f596c258f34c5aa107f78ad883c4e1f550a
LanPodder <alan.i.shref@gmail.com>
2023-01-11 01:20:38 +0100

    forgot a return there

--- src/handler/read-meta.ts
+++ src/handler/read-meta.ts
@@ -3,5 +3,5 @@ import * as http from "http";
 
 export default (account: Account, req: http.IncomingMessage) => {
     account.updateHostName(req.headers.host);
-    account.read();
+    return account.read();
 }
