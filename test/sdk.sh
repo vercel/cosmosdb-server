69ba0ee66ab270a02dda5241910f3005dc36c3aa
Douglas Parsons <dglsparsons@gmail.com>
2023-05-21 17:35:01 +0100

    Override tsconfigs

--- test/sdk.sh
+++ test/sdk.sh
@@ -16,11 +16,18 @@ pid=$!
 cd test/azure-sdk-for-js
 npm i -g @microsoft/rush
 rush update
+
+# Override all `tsconfig.json` to prevent it picking up type definitions from our
+# node_modules (which is a parent dir).
+# Manually filtering a couple of dodgy ones out, because they contain json comments, which jq can't parse
+for f in $(find . -name tsconfig.json | grep -v node_modules); do
+  cp "$f" "$f.bak"
+  cat "$f.bak" | jq '. * { "compilerOptions": { "typeRoots": ["./node_modules/@types/"] }}' > "$f" || echo "skipping $f"
+  rm "$f.bak"
+done
+
 cd sdk/cosmosdb/cosmos
-# Override their `tsconfig` to prevent it picking up type definitions from our node_modules (which is a parent dir).
-mv tsconfig.json tsconfig.json.bak
-cat tsconfig.json.bak | jq '. * { "compilerOptions": { "typeRoots": ["./node_modules/@types/"] }}' > tsconfig.json
 rush build:test -f .
 
 ACCOUNT_HOST="https://localhost:$port" npm run integration-test:node -- --i --exit \
-  -g 'Authorization|http proxy|Change Feed|Partition|indexing|Offer CRUD|Parallel Query As String|Permission|Query Metrics On Single Partition Collection|ResourceLink Trimming|Session Token|spatial|sproc|stored procedure|Trigger|trigger|TTL|User|Non Partitioned|Validate SSL verification|matching constant version & package version|Conflicts|Partition|GROUP BY|.readOffer|autoscale|with v2 container'
+-g 'Authorization|http proxy|Change Feed|Partition|indexing|Offer CRUD|Parallel Query As String|Permission|Query Metrics On Single Partition Collection|ResourceLink Trimming|Session Token|spatial|sproc|stored procedure|Trigger|trigger|TTL|User|Non Partitioned|Validate SSL verification|matching constant version & package version|Conflicts|Partition|GROUP BY|.readOffer|autoscale|with v2 container'
